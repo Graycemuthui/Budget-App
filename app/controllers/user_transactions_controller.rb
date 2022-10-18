9c2246fe8d5293e1a916012acb2392ea7062bed0
Grayce Muthui <graycemuthui20@gmail.com>
2022-10-18 12:30:35 +0300

    style the templates and add methods on user_transactions

--- app/controllers/user_transactions_controller.rb
+++ app/controllers/user_transactions_controller.rb
@@ -1,2 +1,24 @@
 class UserTransactionsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user_transaction
+
+  def index
+    @user_transactions = @group.user_transactions.order(created_at: :desc)
+  end
+
+  def new
+    @user_transaction = UserTransaction.new
+  end
+
+  def create
+    @user_transaction = @group.user_transactions.new(user_transaction_params)
+    @user_transaction.author_id = current_user.id
+    respond_to do |format|
+      if @user_transaction.save
+        format.html { redirect_to group_user_transactions_path, notice: 'User transaction was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
 end
