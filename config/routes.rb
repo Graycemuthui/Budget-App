c9c4dd85412ec3e986fbd61d90bcc5152f59b57d
Grayce Muthui <graycemuthui20@gmail.com>
2022-10-18 11:28:59 +0300

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'users/sign_out' => 'devise/sessions#destroy'
   end
+  resources :groups do
+    resources :user_transactions
+  end
+  root 'groups#index'
 end
